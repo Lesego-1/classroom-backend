1599dc9a25bbe03c7f832c4df831c466d96edd46
Lesego <lesegojuniorrobert7@gmail.com>
2026-01-15 14:18:09 +0200

    implement whatever this is

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import subjectsRouter from './routes/subjects';
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
 
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}))
+
 app.use(express.json());
+
+app.use('/api/subjects', subjectsRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, welcome to the Classroom API!');
@@ -12,3 +22,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
+
